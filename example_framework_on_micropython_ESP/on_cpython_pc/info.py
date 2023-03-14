71c69a37ec53d30d01ce1e771ce1659e40b16982
m24h <jjkkxyz@hotmail.com>
2023-03-15 01:37:18 +0800

    Added RPC commands list

--- example_framework_on_micropython_ESP/on_cpython_pc/info.py
+++ example_framework_on_micropython_ESP/on_cpython_pc/info.py
@@ -7,11 +7,11 @@ import binascii
 async def init():
 	async with await arpc.connect(conf.server['host'], conf.server['port'], password=conf.server['password']) as sess:
 		await sess.exec('import sys')
-		print('sys.implementation : ', await sess.eval('repr(sys.implementation)')) # now upython's json.dumps can dump sys.implementation incorrectly, but cpython's json.loads() fails to load it back
+		print('sys.implementation : ', await sess.eval('repr(sys.implementation)')) # now json.dumps can dump sys.implementation incorrectly, but fails when loads() it
 		await sess.exec('import machine')
 		print('machine.freq :', await sess.eval('machine.freq()'))
 		print('machine.unique_id :', binascii.hexlify((await sess.eval('machine.unique_id()')).encode('iso8859-1')).decode('iso8859-1'))
 		await sess.exec('import micropython')
 		print('micropython.stack_use :', await sess.eval('micropython.stack_use()'))
-		
+		print('RPC :', await sess.list())
 asyncio.run(init())
