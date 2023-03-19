b7fc04e506a4b1c854155c8eedfd57592f0936e7
m24h <jjkkxyz@hotmail.com>
2023-03-19 09:16:24 +0800

    Add files via upload

---
+++ example_framework_on_micropython_ESP/on_cpython_pc/eval.py
@@ -0,0 +1,11 @@
+import arpc
+import asyncio
+import conf
+from sys import argv
+
+async def cmds():
+	async with await arpc.connect(conf.server['host'], conf.server['port'], password=conf.server['password']) as sess:
+		print(await sess.eval(argv[1]))
+
+if __name__=='__main__':
+	asyncio.run(cmds())
